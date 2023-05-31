32ad6504066c86ee7a941e7987c3c6ddaeaba6d0
Bulbsum <garcialopezgerardo6612@gmail.com>
2023-05-30 18:01:32 -0600

    feat: remove loader screen

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,18 @@
 <script setup lang='ts'>
-import { watchEffect, ref } from 'vue'
 import NavBar from './components/NavBar.vue'
 import PersonalInfo from './components/PersonalInfo.vue'
 import ProjectsContainer from './components/ProjectsContainer.vue'
-import ToolsContainer from './components/ToolsContainer.vue'
-import LoadingPage from './components/LoadingPage.vue'
-
-const loading = ref<boolean>(true)
-
-watchEffect(() => {
-  setTimeout(() => {
-    loading.value = false
-  }, 1500)
-})
 </script>
 
 <template>
-  <LoadingPage v-if='loading'/>
-  <template v-else>
-    <NavBar />
-    <PersonalInfo />
-    <main>
-      <ProjectsContainer />
-      <ToolsContainer />
-    </main>
-  </template>
+  <NavBar />
+  <PersonalInfo />
+  <ProjectsContainer />
 </template>
 
 <style scoped>
 nav, section, main {
-  animation: unvanish 1s ease-in-out;
+  animation: unvanish 0.8s ease-in-out;
 }
 
 @keyframes unvanish {
@@ -38,24 +21,6 @@ nav, section, main {
   }
   100% {
     opacity: 1;
-  }
-}
-
-main {
-  display: grid;
-  grid-template-columns: 1fr 10rem 1fr;
-  padding: 0 1rem;
-  gap: 2rem;
-}
-@media (max-width: 860px) {
-  main {
-    grid-template-columns: repeat(2, 1fr);
-  }
-}
-
-@media (max-width: 430px) {
-  main {
-    grid-template-columns: 1fr;
   }
 }
 </style>
