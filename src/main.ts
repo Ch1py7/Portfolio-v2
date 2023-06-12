911098d496d4a963ba55c14e5796715bbdef218d
Ch1py7 <garcialopezgerardo6612@gmail.com>
2023-06-12 13:11:53 -0600

    fix: set async function

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from './App.vue'
 import './assets/main.css'
 import { getApiData } from './graphql/client'
 
-const { apolloProvider } = await getApiData()
+async function initializeApp() {
+  const { apolloProvider } = await getApiData()
+  createApp(App).use(apolloProvider).mount('#app')
+}
 
-createApp(App).use(apolloProvider).mount('#app')
+initializeApp()
