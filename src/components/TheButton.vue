60f27176c20d308c5f7c98674cddabf5d350e790
Ch1py7 <garcialopezgerardo6612@gmail.com>
2023-06-13 23:05:07 -0600

    chore: add new svgs and types

---
+++ src/components/TheButton.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { Ref } from 'vue'
+defineProps<{
+  mode: Ref
+  firstMode: object
+  secMode: object
+  toggle: () => void
+}>()
+</script>
+
+<template>
+  <button
+    ttitle="button"
+    type="button"
+    @click="toggle"
+    :class="{ active: mode.value === true }"
+  >
+    <span :class="{ anim: mode.value === true }">
+      <component :is="mode.value ? firstMode : secMode" />
+    </span>
+  </button>
+</template>
+
+<style scoped>
+button {
+  position: relative;
+  display: flex;
+  align-items: center;
+  height: 2.4rem;
+  width: 4.4rem;
+  border: solid 1px #343434;
+  border-radius: 2.2rem;
+  background-color: #2b2b2b;
+}
+
+span {
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 2rem;
+  width: 2rem;
+  transition: transform 0.2s linear;
+}
+
+span.anim {
+  transform: translateX(2rem);
+}
+
+button.active {
+  background-color: #bbb;
+}
+</style>
